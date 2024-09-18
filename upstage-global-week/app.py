f3a1efb8153768244626b3f2a762443c43842af5
venomsnaker <qhuy.bui.developer@gmail.com>
2024-09-18 21:15:35 +0700

    Clean Up and Update places Database

--- upstage-global-week/app.py
+++ upstage-global-week/app.py
@@ -1,38 +1,64 @@
-import gradio as gr
+# Import Libraries
 import os
 import warnings
 warnings.filterwarnings("ignore")
 from dotenv import load_dotenv
+
 from typing import List, TypedDict
+import gradio as gr
+
 from langchain_upstage import ChatUpstage, UpstageLayoutAnalysisLoader, UpstageEmbeddings, GroundednessCheck
 from langchain_core.output_parsers import StrOutputParser
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.vectorstores import VectorStore
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
-from langchain_core.vectorstores import VectorStore
+from langchain.prompts import ChatPromptTemplate
 from langgraph.graph import END, StateGraph
 
+# Set up Upstage API
 upstage_api_key_env_name = "UPSTAGE_API_KEY"
 def load_env():
     load_dotenv()
     return os.environ.get(upstage_api_key_env_name)
 UPSTAGE_API_KEY = load_env()
 
+# Set up the database
 def layout_analysis(filenames: str) -> List[Document]:
     layout_analysis_loader = UpstageLayoutAnalysisLoader(filenames, split="element")
     return layout_analysis_loader.load()
 
 filenames = [
     "data/CS224n-Week1.pdf",
+    "data/CS224n-Week2.pdf",
+    "data/CS224n-Week3.pdf",
+    "data/CS224n-Week4.pdf",
+    "data/CS224n-Week5.pdf",
+    "data/CS224n-Week6.pdf",
+    "data/CS224n-Week7.pdf",
+    "data/CS224n-Week8.pdf",
+    "data/CS224n-Week9.pdf",
+    "data/CS224n-Week10.pdf",
 ]
 docs = layout_analysis(filenames)
 db: VectorStore = Chroma(
     embedding_function=UpstageEmbeddings(model="solar-embedding-1-large-passage")
 )
 retriever = db.as_retriever()
-db.add_documents(docs)
+# db.add_documents(docs)
 
-class RagState(TypeDict):
+# Set up the model and prompt
+template = """Answer the question based only on the given context.
+{context}
+
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+model = ChatUpstage()
+model_chain = prompt | model | StrOutputParser()
+
+# Set up the RAG system
+class RagState(TypedDict):
     """
     Represents the state of our graph.
 
@@ -48,15 +74,6 @@ class RagState(TypeDict):
     answer: str
     groundedness: str
 
-template = """Answer the question based only on the given context.
-{context}
-
-Question: {question}
-"""
-prompt = ChatPromptTemplate.from_template(template)
-llm = ChatUpstage(model="solar-pro")
-llm_chain = prompt | llm | StrOutputParser()
-
 def format_documents(docs: List[Document]) -> str:
     return "\n".join([doc.page_content for doc in docs])
 
@@ -66,51 +83,53 @@ def retrieve(state: RagState) -> RagState:
     return RagState(context=context)
 
 def model_answer(state: RagState) -> RagState:
-    response = llm_chain.invoke(state)
-    return RagState(answer=response)
+    response = model_chain.invoke(state)
+    return {**state, "answer": response}
 
-prompt = ChatPromptTemplate.from_template(template)
 gc = GroundednessCheck()
 
-def groundedness_check(stage: RagState) -> RagState:
-    response = gc.run({'context': state["context"], "answer": state["answer"]})
-    return RagState(groundedness_check=response)
+def groundedness_check(state: RagState) -> RagState:
+    response = gc.run({"context": state["context"], "answer": state["answer"]})
+    return RagState(groundedness=response)
 
 def groundedness_condition(state: RagState) -> RagState:
     return state["groundedness"]
 
+# Set up the workflow
 workflow = StateGraph(RagState)
 workflow.add_node("retrieve", retrieve)
 workflow.add_node("model", model_answer)
 workflow.add_node("groundedness_check", groundedness_check)
-workflow.add_edge("retrive", "model")
-workflow.add_edge("model", "groundedness_heck")
-workflow.add_conditonal_edges(
+
+workflow.add_edge("retrieve", "model")
+workflow.add_edge("model", "groundedness_check")
+workflow.add_conditional_edges(
     "groundedness_check",
     groundedness_condition,
     {
         "grounded": END,
         "notGrounded": "model",
-        "notSure": "model"
-    }
+        "notSure": "model",
+    },
 )
 workflow.set_entry_point("retrieve")
 app = workflow.compile()
 
 def chat(inputs_user):
     inputs = {"question": inputs_user}
-    output = app.stream(inputs)
-    print(type(output))
-    print(output[1])
-    return "";    
-    # result = llm.invoke(input)
-    # return result.content   
+    respond = ""
+
+    for output in app.stream(inputs):
+        for key, value in output.items():
+            if key == 'model' and 'answer' in value:
+                respond = value['answer']
+    return respond
 
 interface = gr.Interface(
     fn=chat,
     inputs=[gr.Textbox(label="text", lines=2)],
     outputs='text',
-    title="Upstage Solar Chatbot 0.2",
+    title="Upstage Solar RAG Chatbot 0.2",
 )
 
 if __name__ == "__main__":
